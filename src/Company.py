3d3294371a9e7ee495a8d36ec7478cc08d68820d
Tiago Portaluppi <tiagoportaluppi.tp@gmail.com>
2020-06-15 16:37:35 -0300

    Distancia euclidiana

--- src/Company.py
+++ src/Company.py
@@ -1,8 +1,26 @@
+from scipy.spatial import distance
+
+
 class Company:
-    def __init__(self, name, latitude, longitude):
+    current_company = []
+
+    def _init_(self, name, latitude, longitude):
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
 
-    def __str__(self):
-        return self.name + " - Latitude: " + self.latitude + " Longitude: " + self.longitude
+    def _str_(self):
+        message = ''
+
+        for company in self.current_company:
+            message += company['company'].name + ' - Distância: ' + str(company['euclidean_distance']) + ' '
+
+        return self.name + " - Latitude: " + self.latitude + " Longitude: " + self.longitude + '\n [' + message + ']\n'
+
+    def euclidean_distance(self, client_point):
+        company_point = (float(self.latitude), float(self.longitude))
+        total_euclidean_distance = distance.euclidean(client_point, company_point)
+        return total_euclidean_distance
+
+    def set_current_company(self, company):
+        self.current_company = company
--- src/principal.py
+++ src/principal.py
@@ -1,7 +1,7 @@
 import os.path
 from src.Company import Company
 
-my_path = os.path.abspath(os.path.dirname(__file__))
+my_path = os.path.abspath(os.path.dirname(_file_))
 clientPath = os.path.join(my_path, '../files/clients.txt')
 
 clientsFile = open(clientPath, 'r')
@@ -21,8 +21,27 @@ providers = providersFile.readlines()
 
 for provider in providers:
     objectProvider = provider.split(' ')
-    providersList.append(Company(objectClient[0], objectClient[1], objectClient[2]))
+    providersList.append(Company(objectProvider[0], objectProvider[1], objectProvider[2]))
 
-print(len(clientsList))
-print(len(providersList))
+count = 0
 
+for client in clientsList:
+    clientPoint = (float(client.latitude), float(client.longitude))
+
+    provider = providersList[0]
+    euclideanDistance = provider.euclidean_distance(clientPoint)
+
+    smallerProvider = {
+      "company": providersList[0],
+      "euclidean_distance": euclideanDistance,
+    }
+
+    for i in range(1, len(providersList)):
+        provider = providersList[i]
+        euclideanDistance = provider.euclidean_distance(clientPoint)
+
+        if smallerProvider['euclidean_distance'] > euclideanDistance:
+            smallerProvider['company'] = provider
+            smallerProvider['euclidean_distance'] = euclideanDistance
+
+    client.set_current_company([smallerProvider])
